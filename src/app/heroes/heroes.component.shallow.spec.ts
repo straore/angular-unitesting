01e3a8c2b0620f8ed1f43044e8b4f02061855583
Siaka TRAORE <Siaka.traore@rte-france.com>
2019-04-01 14:56:09 +0200

    shallow integ test mocking the service,using fake component and ignoring routerLink

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,26 +1,41 @@
 import { TestBed, ComponentFixture } from "@angular/core/testing";
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NO_ERRORS_SCHEMA, Input, Component } from "@angular/core";
 import { By } from "@angular/platform-browser";
 import { HeroesComponent } from "./heroes.component";
 import { of } from "rxjs";
 import { HeroService } from "../hero.service";
+import { Hero } from "../hero";
 
 describe('HeroesComponent (shallow tests)',() => {
 let fixture: ComponentFixture<HeroesComponent>;
 let mockHeroService= jasmine.createSpyObj(['getHeroes','addHero','deleteHero']);
-let HEROES = [{id:1, name:'SuperDude', strength: 3},
-{id:1, name:'SuperDude', strength: 3},
-{id:1, name:'SuperDude', strength: 3}]
+let HEROES;
+    //mock class
+    @Component({
+        selector: 'app-hero',
+        template: '<div></div>',
+      })
+    class FakeHeroComponent {
+        @Input() hero: Hero;
+    }
+
+
 beforeEach(() => {
+    HEROES = [
+        {id:1, name:'SpiderDude', strength: 8},
+        {id:2, name:'Batman', strength: 24},
+        {id:3, name:'SuperDude', strength: 50}
+    ];
+
         TestBed.configureTestingModule({
-            declarations:[HeroesComponent],
+            declarations:[HeroesComponent,FakeHeroComponent],
             providers:[
                 //long hand provider syntax
             {provide: HeroService,useValue: mockHeroService}
         ],
             //ignore the child component and the child directive(routerLink) with this
             //Side effect: will hide issues in the template
-            schemas:[NO_ERRORS_SCHEMA]
+            //schemas:[NO_ERRORS_SCHEMA]
         });
        
         fixture = TestBed.createComponent(HeroesComponent);
